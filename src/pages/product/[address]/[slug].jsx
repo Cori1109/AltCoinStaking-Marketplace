a2d9f52ac14117f3a61357f29c5409b6a47afd77
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-05-06 07:33:36 -0400

    fixed the dynamic router issue

---
+++ src/pages/product/[address]/[slug].jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import SEO from "@components/seo";
+import Wrapper from "@layout/wrapper";
+import Header from "@layout/header";
+import Footer from "@layout/footer";
+import Breadcrumb from "@components/breadcrumb";
+import ProductDetailsArea from "@containers/product-details";
+
+const ProductDetails = ({ products }) => {
+    return (
+        <Wrapper>
+            <SEO pageTitle="Product Details" />
+            <Header />
+            <main id="main-content">
+                <Breadcrumb
+                    pageTitle="Product Details"
+                    currentPage="Product Details"
+                />
+                <ProductDetailsArea product={products} />
+            </main>
+            <Footer />
+        </Wrapper>
+    );
+};
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`http://localhost:3000/api/marketItem`);
+    const result = await res.json();
+    const products = result.data;
+
+    return { props: { products } };
+}
+
+ProductDetails.propTypes = {
+    products: PropTypes.shape({}),
+};
+
+export default ProductDetails;
